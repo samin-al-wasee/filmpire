599f6231390210cffefeaf3822d3ad9889c8f2cc
Rafeed <sameenwasee@gmail.com>
2023-07-06 23:57:40 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// API Key = 841b2a9e
+
+const API_URL = "http://www.omdbapi.com?apikey=841b2a9e"
+
+const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+};
+
+const App = () => {
+    return (
+        <h1>FILMPIRE HOME</h1>
+    );
+};
 
 export default App;
